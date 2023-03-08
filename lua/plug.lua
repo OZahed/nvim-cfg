d1fd289d4fc63caa96675c6bc6fe71d06f90537d
omid.zahed <omid.zahed@snapp.cab>
2023-03-08 13:45:55 +0330

    vim-cmp added

---
+++ lua/plug.lua
@@ -0,0 +1,82 @@
+-- [[ plug.lua ]]
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    print("Installing packer close and reopen Neovim...")
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerSync
+augroup end
+]])
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init({
+    display = {
+        open_fn = function()
+            return require('packer.util').float({ border = 'single' })
+        end
+    }
+}
+)
+
+-- Install your plugins here
+return packer.startup(function(use)
+    -- My plugins here
+    use 'wbthomason/packer.nvim' -- Have packer manage itself
+    use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+    use {                                              -- filesystem navigation
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons'        -- filesystem icons
+    }
+    -- [[ Theme ]]
+    use { 'mhinz/vim-startify' }                       -- start screen
+    use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
+    use {
+        'nvim-lualine/lualine.nvim',                     -- statusline
+        requires = {'kyazdani42/nvim-web-devicons',
+            opt = true}
+    }
+    use { 'Mofiqul/dracula.nvim' }
+    -- [[ Dev ]]
+    use {
+        'nvim-telescope/telescope.nvim',                 -- fuzzy finder
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { 'majutsushi/tagbar' }                        -- code structure
+    use { 'Yggdroot/indentLine' }                      -- see indentation
+    use { 'tpope/vim-fugitive' }                       -- git integration
+    use { 'junegunn/gv.vim' }                          -- commit history
+    use { 'windwp/nvim-autopairs' }  
+    use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    use "neovim/nvim-lspconfig" -- enable LSP
+    use "williamboman/mason.nvim"
+    use "williamboman/mason-lspconfig.nvim"
+    use "jose-elias-alvarez/null-ls.nvim"               -- for formatters and linters
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-buffer"
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'honza/vim-snippets'
+    use 'dcampos/nvim-snippy'
+    use 'dcampos/cmp-snippy'
+
+if PACKER_BOOTSTRAP then
+    require('packer').sync()
+end
+end)
---
+++ lua/user/cmp.lua
@@ -0,0 +1,73 @@
+local cmp = require'cmp'
+
+require('snippy').setup({
+    mappings = {
+        is = {
+            ['<Tab>'] = 'expand_or_advance',
+            ['<S-Tab>'] = 'previous',
+        },
+        nx = {
+            ['<leader>x'] = 'cut_text',
+        },
+    },
+})
+
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-d>"] = cmp .mapping. scroll_docs (-4),
+        ['<C-f>'] = cmp. mapping. scroll_docs(4),
+        ['<C-o>'] = cmp. mapping. complete( ),
+        ['<C-e>'] = cmp. mapping. close() ,
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+            { name = 'buffer' },
+        })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = {
+        {name = 'nvim_lsp'},
+        {name = 'look'},
+        {name= "buffer"},
+    } 
+})
+
+local lsp = require 'lspconfig'
+local cmp_lsp = require 'cmp_nvim_lsp'
+
+lsp.vimls.setup {
+    capabilities = cmp_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+
+    require('lspconfig')['pyright'].setup {
+        capabilities = capabilities
+    },
+    require('lspconfig')['gopls'].setup {
+        capabilities = capabilities
+    },
+    require('lspconfig')['clangd'].setup {
+        capabilities = capabilities
+    },
+}
+
